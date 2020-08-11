8f92baaab7fff5b2f833baec1bc4f8986e15edb5
Виталий <wental-@mail.ru>
2020-08-12 00:35:30 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+$app->get('/test', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("test");
+    return $response;
+});
+
+$app->run();
